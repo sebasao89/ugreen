0796cbc3f48e5aeae6061c47391e9bf9e9be0384
sebas-meseta <sebas.ao@outlook.com>
2023-12-06 17:25:55 -0500

    deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sebasao89.github.io',
+  base: '/dist',
   integrations: [tailwind()]
 });
